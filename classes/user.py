fc929b2d2c4f3f20a39670ab4c4014339336a77a
FloWambui <gflorencewambui@gmail.com>
2022-04-25 11:04:25 +0300

    this commit will add the save user object to the list

---
+++ classes/user.py
@@ -0,0 +1,24 @@
+class User:
+    """
+    class generating a new instance of users
+    """
+    user_details = []
+
+    def _init_(self, first_name, last_name, username, password):
+        """
+        properties for the password lock object
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+     
+    #  Saving User
+
+    def save_user(self):
+        """
+        save_user method saves user to user_details list
+        """
+
+        User.user_details.append(self)
